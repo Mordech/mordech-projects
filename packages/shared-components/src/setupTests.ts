fe9541f688b7071e32fd73d951c79993b8c8ae0a
Elad Mizrahi <elad@mizrahi.cc>
2022-10-15 12:32:38 +0300

    chore(components): migrate to vitest from jest

--- packages/shared-components/src/setupTests.ts
+++ packages/shared-components/src/setupTests.ts
@@ -1,9 +1,10 @@
+import { vitest, afterEach } from 'vitest';
+import { toHaveNoViolations } from 'jest-axe';
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-styled-components';
-import { toHaveNoViolations } from 'jest-axe';
 
 expect.extend(toHaveNoViolations);
 
 afterEach(() => {
-  jest.clearAllMocks();
+  vitest.clearAllMocks();
 });
