6460075ccaa71d71aa9744881ca7eb741c080215
Elad Mizrahi <elad@mizrahi.cc>
2022-10-15 12:24:01 +0300

    feat: upgrade markdown version

--- packages/shared-components/src/lib/components/Markdown/Markdown.spec.tsx
+++ packages/shared-components/src/lib/components/Markdown/Markdown.spec.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import { Markdown } from './Markdown';
+import { explanationText } from './story/consts';
+import { axe } from 'jest-axe';
+// TODO write tests
 
 describe('Markdown', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Markdown>dsa</Markdown>);
+  it('should render successfully', async () => {
+    const { baseElement } = render(<Markdown>{explanationText}</Markdown>);
     expect(baseElement).toBeTruthy();
+    expect(await axe(baseElement)).toHaveNoViolations;
   });
 });
