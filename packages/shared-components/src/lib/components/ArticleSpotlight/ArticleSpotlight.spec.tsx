c7497180ef3553e24e5e4d26885987b4168f7cb2
Elad Mizrahi <elad@mizrahi.cc>
2022-10-16 17:21:22 +0300

    feat(shared-components): support react element as a thumbnail option

--- packages/shared-components/src/lib/components/ArticleSpotlight/ArticleSpotlight.spec.tsx
+++ packages/shared-components/src/lib/components/ArticleSpotlight/ArticleSpotlight.spec.tsx
@@ -21,4 +21,21 @@ describe('ArticleSpotlight', () => {
     expect(baseElement).toBeTruthy();
     expect(await axe(baseElement)).toHaveNoViolations;
   });
+  it('should render with a react element as thumbnail', async () => {
+    const { getByTestId } = render(
+      <ArticleSpotlight
+        href="#"
+        headline="**Lorem Ipsum dollor:** Fusce at dolor sed nunc"
+        description={sampleDescription}
+        thumbnailSrc={
+          <img
+            data-testid="thumbnail"
+            src="https://images.unsplash.com/photo-1615678815958-5910c6811c25?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=MnwxfDB8MXxyYW5kb218MHx8Y2F0fHx8fHx8MTY2MjkyMzI4NQ&ixlib=rb-1.2.1&q=80&w=1600"
+            alt=""
+          />
+        }
+      />
+    );
+    expect(getByTestId('thumbnail')).toBeInstanceOf(HTMLImageElement);
+  });
 });
