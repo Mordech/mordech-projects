fc496f7a34208cc65529552184739006e9c7907f
Elad Mordechai Mizrahi <elad@mizrahi.cc>
2022-11-20 19:12:54 +0200

    chore: upgrade to latest nx and gatsby

--- apps/portfolio/gatsby-config.ts
+++ apps/portfolio/gatsby-config.ts
@@ -10,7 +10,6 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    'gatsby-plugin-netlify',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-image',
     'gatsby-plugin-catch-links',
