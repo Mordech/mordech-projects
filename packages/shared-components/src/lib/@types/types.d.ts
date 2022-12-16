6acc06e2d61d403abf7a85764e95c80cbfa217f9
Elad Mordechai Mizrahi <elad@mizrahi.cc>
2022-12-16 22:10:33 +0200

    chore: remove redundent type declaration

--- packages/shared-components/src/lib/@types/types.d.ts
+++ packages/shared-components/src/lib/@types/types.d.ts
@@ -1,6 +1,2 @@
 export * from '@testing-library/jest-dom';
 export * from 'jest-styled-components';
-declare module '*.svg';
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
