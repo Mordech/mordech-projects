7b089e7ed00ec87627b0bf28dccb7b81a713141d
Elad Mordechai Mizrahi <71976177+Mordech@users.noreply.github.com>
2024-01-02 20:32:32 +0200

    test(vite-lit-loader): add tests (#119)

---
+++ packages/vite-lit-loader/vite.config.ts
@@ -0,0 +1,25 @@
+/// <reference types='vitest' />
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/packages/vite-lit-loader',
+
+  plugins: [nxViteTsPaths()],
+
+  test: {
+    globals: true,
+    cache: {
+      dir: '../../node_modules/.vitest',
+    },
+    environment: 'node',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '/packages/vite-lit-loader/coverage',
+      provider: 'v8',
+    },
+  },
+});
